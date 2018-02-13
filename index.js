c78dfdef6ee16554a334897c96cfbd6551dcb638
osimba <osiqua@gmail.com>
2018-02-12 23:18:48 -0700

    completed findToys and findAnimals

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-//var Animal = require('./Animal.js');
+var Animal = require('./Animal.js');
 var Toy = require('./Toy.js');
 
 
@@ -10,9 +10,19 @@ app.get('/', (req, res) => {
 	res.write('Hello Worlds this is Osei!');
 
     });
+/*
+app.get('/addToy', (req, res) => {
+	var newToy = new Toy({
+		id : "123",
+		name : "Dog chew toy",
+		price : 10.99
+	});
+
+	newToy.save();
+	console.log('new toy added');
+});*/
 
 app.get('/findToy', (req, res) => {
-	res.write('This is the findToys page');
 	var itemQuery = req.query.id;
 
 	//fetch name and price from mongo db
@@ -24,20 +34,50 @@ app.get('/findToy', (req, res) => {
 		else if (!toy) {
 			res.type('html').status(200);
 			res.send('There is no toy with id ' + itemQuery);
+			return toy;
 		}
 		else {
 			//return entire document object
-			console.log(toy);
+			res.json(toy);
 		}
 	});
 });
 
+app.get('/addAnimal', (req, res) => {
+	var newAnimal = new Animal({
+		name : "Cooper",
+		species : "Dog",
+		breed : "Catahoula",
+		gender : "male",
+		age : 11,
+		traits : ["crazy", "funny"]
+	});
+
+	newAnimal.save();
+	console.log('new animal added');
+});
+
 app.get('/findAnimals', (req, res) => {
-	res.write('This is the findAnimals page');
+	Animal.find(req.query, (err, animals) => {
+		if(err) {
+			res.type('html').status(500);
+			res.send('Error: ' + err);
+		}
+		else {
+			res.json(animals);
+		}
+	});
 });
 
 app.get('/animalsYoungerThan', (req, res) => {
-	res.write('This is the animalsYoungerThan page');
+	//get animals
+
+	//set values
+	var animalsObj = {
+		count: 0
+	};
+
+
 });
 
 app.get('/calculatePrice', (req, res) => {
